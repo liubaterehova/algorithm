dde9a23fd72971d73f4823befc4d21061ae613e6
Liubou Tserakhava <Liubou.Tserakhava@itechart-group.com>
2020-01-16 14:53:38 +0300

    last task

--- src/tasks/six/func.js
+++ src/tasks/six/func.js
@@ -1,3 +1,17 @@
+convertFrom2 = (number) => {
+  if (number.includes('0') && number.includes('1')) {
+    let res = 0;
+    const arrOfNum = number.split('');
+    arrOfNum.forEach((num, index) => {
+      res += num * 2 ** index;
+    });
+
+    return res;
+  }
+
+  return 'mistake';
+}
+
 const converter = {
     convertFrom2To10: number => {
         let res = 0;
